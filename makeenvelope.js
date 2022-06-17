e11eaaca7000d205ebb7f7718dcb2d3638f16966
Rich <76711805+ribo916@users.noreply.github.com>
2022-06-17 01:21:29 +0000

    working to test larger packages

--- makeenvelope.js
+++ makeenvelope.js
@@ -6,19 +6,56 @@ function makeEnvelope(args) {
   let env = new docusign.EnvelopeDefinition();
   env.emailSubject = "DocuSign REPLIT Example";
 
-  let doc1, doc2, doc3, doc4;
+  let doc1, doc2, doc3, doc4, doc5, doc6, doc7, doc8, doc9, doc10, doc11;
   if (args.scenario == 1) {
     doc1 = createHtmlDefinitionDoc('CreditScoreSummary.html');
     doc1.documentId = "1";
     doc2 = createHtmlDefinitionDoc('Odometer.html');
     doc2.documentId = "2";
-    env.documents = [doc1, doc2];
+    doc3 = createHtmlDefinitionDoc('AgreementNotToExport.html');
+    doc3.documentId = "3";
+    doc4 = createHtmlDefinitionDoc('AgreementToProvideIns.html');
+    doc4.documentId = "4";
+    doc5 = createHtmlDefinitionDoc('ConsumerLoan.html');
+    doc5.documentId = "5";
+    doc6 = createHtmlDefinitionDoc('DCAP_CO.html');
+    doc6.documentId = "6";
+    doc7 = createHtmlDefinitionDoc('FactAct.html');
+    doc7.documentId = "7";
+    doc8 = createHtmlDefinitionDoc('Law_Contract.html');
+    doc8.documentId = "8";
+    doc9 = createHtmlDefinitionDoc('RetailSpotDelivery.html');
+    doc9.documentId = "9";
+    doc10 = createHtmlDefinitionDoc('WA_Payoff_Release.html');
+    doc10.documentId = "10";
+    doc11 = createHtmlDefinitionDoc('WA_DMV_LemonLaw.html');
+    doc11.documentId = "11";
+    env.documents = [doc1, doc2, doc3, doc4, doc5, doc6, doc7, doc8, doc9, doc10, doc11];
   } else if (args.scenario == 2) {
-    doc1 = readPdfDoc("WeOwe.pdf");
+    doc1 = readPdfDoc("CreditScoreSummary.pdf");
     doc1.documentId = "1"; 
-    doc2 = readPdfDoc("W9.pdf");
+    doc2 = readPdfDoc("OdometerStatement.pdf");
     doc2.documentId = "2";
-    env.documents = [doc1, doc2];    
+    doc3 = readPdfDoc('AgreementNotToExport.pdf');
+    doc3.documentId = "3";
+    doc4 = readPdfDoc('AgreementToProvideIns.pdf');
+    doc4.documentId = "4";
+    doc5 = readPdfDoc('ConsumerLoan.pdf');
+    doc5.documentId = "5";
+    doc6 = readPdfDoc('91581_DCAP_CO.pdf');
+    doc6.documentId = "6";
+    doc7 = readPdfDoc('FactActNotice.pdf');
+    doc7.documentId = "7";
+    doc8 = readPdfDoc('43894_Law_Contract.pdf');
+    doc8.documentId = "8";
+    doc9 = readPdfDoc('RetailSpotDelivery.pdf');
+    doc9.documentId = "9";
+    doc10 = readPdfDoc('85821_WA_Payoff_Release.pdf');
+    doc10.documentId = "10";
+    doc11 = readPdfDoc('88352_WA_DMV_LemonLaw.pdf');
+    doc11.documentId = "11";
+    
+    env.documents = [doc1, doc2, doc3, doc4, doc5, doc6, doc7, doc8, doc9, doc10, doc11];    
   } else {
     doc1 = createHtmlDefinitionDoc('CreditScoreSummary.html');
     doc1.documentId = "1";
@@ -80,7 +117,7 @@ function makeEnvelope(args) {
   // Configure webhook
   let eventNotification = new docusign.EventNotification();
   // Set up the endpoint URL to call (it must be using HTTPS and at least TLS1.1 or higher)
-  eventNotification.url = 'https://webhook.site/2195d8cf-1a7f-4d6a-91a8-f0c54ea62bf2';
+  eventNotification.url = 'https://webhook.site/16c560e6-92c5-431f-8b0c-47c89865e475';
   // DocuSign will retry on failure if this is set
   eventNotification.requireAcknowledgment = 'false';
   // This would send the documents together with the event to the endpoint
